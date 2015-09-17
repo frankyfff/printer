816030c6bc3e8920465b3e5df51b393ce90b872e
Druckertester <Druckertester@Windows8_1>
2015-09-17 15:11:00 +0200

    working version

--- brobsi/src/brobsi/Anmeldung.java
+++ brobsi/src/brobsi/Anmeldung.java
@@ -1,5 +1,7 @@
 package brobsi;
 
+import javax.swing.text.View;
+import javax.swing.text.html.ImageView;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,67 +9,76 @@ import javax.ws.rs.Produces;
 @Path("anmeldung")
 public class Anmeldung {
 	
+	
+	
 	@POST
 	@Produces("application/xml")
 	public String login(){
 		return 		"<?xml version='1.0' encoding='utf-8'?>" + 
-			"<SerioCommands version='v009'>" + 
+			"<SerioCommands version='1.1'>" + 
 				"<DisplayForm>" + 
 					"<Script>" + 
-						"<![CDATA[" +
-							"<UiScreen>" + 
-//								"<Title>Welcome To BSI</Title>" + 
-//									"<LinkScreen>" + 
-//										
-//										"<LinkControl>" + 
-//											
-//											"<LinkItem href='./1.xml'>"+
-//												"<Label>Drucken</Label>"+ 
-//											"</LinkItem>" + 
-//											
-//											"<LinkItem href='http://www.172.16.100.196:8080/brobsi/brobsi/scannen'>"+
-//												"<Label>Scannen</Label>" + 
-//											"</LinkItem>"+ 
+						"<![CDATA["+
+							"<UiScreen>"+ 
+					        	
+//								"<Operations>"+
+//				          			"<Op action='./6.xml' type='Submit'></Op>"+
+//				          			"<Op action='./5.xml' type='Back'></Op>"+
+//				          		"</Operations>"+
+//
+//								"<IoScreen>"+
+//									"<IoObject>"+ 
+//										"<Selection id='1' multiple='false'>"+ 
+//
+//			"<Item value='one'>"+
+//				"<Label imgsrc='./brobsi/WebContent/image/printer.png' imgmethod='GET'></Label>"+
+//			"</Item>"+
+//			"<Item value='two'>"+
+//				"<Label></Label>"+
+//			"</Item>"+
+//        "<Item value='three'>"+
+//           "<Label></Label>"+
+//        "</Item>"+
+//           
+//        "<Item value='four'>"+
+//           "<Label></Label>"+
+//         "</Item>"+
+//										"</Selection>"+ 
+//
 //									
-//											"<LinkItem href='./3.xml'>"+
-//												"<Label>Faxen</Label>" + 
-//											"</LinkItem>"+ 
-//										
-//											"<LinkItem href='./4.xml'>"+
-//												"<Label>Copy</Label>"+ 
-//											"</LinkItem>"+ 
-//											
-//									"</LinkControl>" + 
-//								"</LinkScreen>" + 
+//									
+//									
+//									"</IoObject>"+ 
+//								"</IoScreen>"+
 
+							
+                     "<Title>Select  function</Title>"+
+                    "<LinkScreen>"+
+                         "<LinkControl>"+
+                            
+                         	"<LinkItem href='./gotoa.xml'>"+
+                                 "<Label imgsrc='./image/brother_bsi.png'></Label>"+
+                                "<Details><Detail>DRUCKEN</Detail></Details>"+
+                             "</LinkItem>"+
+                                
+							"<LinkItem href='./gotoa.xml'>"+
+								"<Label imgsrc='./brobsi/image/brother_bsi.png'></Label>"+
+								"<Details><Detail>KOPIEREN</Detail></Details>"+
+							"</LinkItem>"+
+                                
+  							"<LinkItem href='./gotoa.xml'>"+
+  								"<Label imgsrc='./brother_bsi.png'></Label>"+
+  								"<Details><Detail>SCANNEN</Detail></Details>"+
+  							"</LinkItem>"+
+  								
+							"<LinkItem href='./gotoa.xml'>"+
+								"<Label imgsrc='./brobsi/image/brother_bsi.png'></Label>"+
+								"<Details><Detail>FAX</Detail></Details>"+
+							"</LinkItem>"+
+  							
+                         "</LinkControl>"+
+                     "</LinkScreen>"+
 
-								
-								
-								
- 								"<LinkControl>"+
- 								"<LinkItem href='http://localhost:8080/brobsi/brobsi/monir.xml'>"+
-		 						"<Label imgsrc='./priner.png'></Label>"+
-                                "<Details><Detail>A.jpg</Detail></Details>"+
-                                "</LinkItem>"+
-                                "<LinkItem href='http://localhost:8080/brobsi/brobsi/monir.xml'>"+
-                                "<Label imgsrc='./priner.png'></Label>"+
-                                "<Details><Detail>B.jpg</Detail></Details>"+
-                                "</LinkItem>"+
-                                "<LinkItem href='http://localhost:8080/brobsi/brobsi/monir.xml'>"+
-                                "<Label imgsrc='./priner.png'></Label>"+
-                                "<Details><Detail>C.jpg</Detail></Details>"+
-                                "</LinkItem>"+
-                                "<LinkItem href='http://localhost:8080/brobsi/brobsi/monir.xml'>"+
-                                "<Label imgsrc='./priner.png'></Label>"+
-                                "<Details><Detail>D.jpg</Detail></Details>"+
-                                "</LinkItem>"+
-                        "</LinkControl>"+
-
-								
-								
-								
-
-						
 							"</UiScreen>" +
 						"]]>" + 
 					"</Script>" + 
