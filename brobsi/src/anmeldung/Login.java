866383b987dae38ed8471c136adc0916235eb79d
Druckertester <Druckertester@Windows8_1>
2015-09-18 16:15:33 +0200

    aktuell

--- brobsi/src/anmeldung/Login.java
+++ brobsi/src/anmeldung/Login.java
@@ -1,12 +1,13 @@
 package anmeldung;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @Path("login")
 public class Login {
-	
+String link="http://172.16.100.196:8080/brobsi/brobsi/anmeldung";	
 	@POST
 	@Produces("application/xml")
 	public String login(){
@@ -16,26 +17,21 @@ public class Login {
 							"<Script>" + 
 								"<![CDATA["+
 									"<UiScreen>"+ 
-											"<Operations>"+
-												"<Op type='Submit' action='./6.xml'></Op>"+
-												"<Op type='Back' action='./5.xml'></Op>"+
-											"</Operations>"+
-	
-											"<IoScreen>"+
-												"<IoObject>"+
-													"<Selection id='1' multiple='false'>"+
-														"<Item value='one'>"+
-															"<Label>Einloggen</Label>"+
-														"</Item>"+
-														"<Item value='two'>"+
-															"<Label>ID-Karte</Label>"+
-														"</Item>"+
-														"<Item value='three'>"+
-															"<Label>Gast-Zugang</Label>"+
-														"</Item>"+
-													"</Selection>"+ 
-												"</IoObject>"+ 
-											"</IoScreen>"+
+						            	"<Title>Bitte wählen Sie?</Title>"+
+						            		"<LinkScreen>"+
+						            			"<LinkControl>"+
+						            				"<LinkItem href='http://172.16.100.196:8080/brobsi/brobsi/anmeldung'>"+
+						            					"<Label>Einloggen</Label>"+
+						            					//"<Href>http://172.16.100.196:8080/brobsi/brobsi/anmeldung<Href>"+
+						            				"</LinkItem>"+
+						            				"<LinkItem href='./2.xml'>"+
+						            					"<Label>ID-Karte</Label>"+
+						            				"</LinkItem>"+
+						            				"<LinkItem href='./3.xml'>"+
+						            					"<Label>Gast</Label>"+
+						            				"</LinkItem>"+
+						            			"</LinkControl>"+
+						            		"</LinkScreen>"+
 									"</UiScreen>" +
 								"]]>" + 
 							"</Script>" + 
--- brobsi/src/brobsi/Anmeldung.java
+++ brobsi/src/brobsi/Anmeldung.java
@@ -2,6 +2,7 @@ package brobsi;
 
 import javax.swing.text.View;
 import javax.swing.text.html.ImageView;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
