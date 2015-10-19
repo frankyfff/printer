8483dbbcd4bbb0f59746450dcd123ec8c0211564
Druckertester <Druckertester@Windows8_1>
2015-10-19 11:27:25 +0200

    new version

--- brobsi/src/detlef/Login.java
+++ brobsi/src/detlef/Login.java
@@ -6,39 +6,110 @@ import javax.ws.rs.Produces;
 
 @Path("loginnew")
 public class Login {
-
+	
 	@POST
 	@Produces("application/xml")
+	@Path("scanning")
+	public String versuch(){
+		return
+				"<?xml version='1.0' encoding='utf-8'?>" + 
+				"<SerioCommands version='1.0'>" + 
+					"<IoScanAndSend>"+
+						"<TxProfiles>"+
+							"<Cifs>"+
+								"<CifsParams>"+
+								"<Host>172.16.100.156</Host>"+
+								"<StoreDir>Ablage</StoreDir>"+
+								"<FileName>ÆÆÆÆÆÆÆÆÆx</FileName>"+
+								"<FileNameFixed>true</FileNameFixed>"+
+								"<AuthMethod>Auto</AuthMethod>"+
+								"<User>Druckertester</User>"+
+								"<Password>drucken</Password>"+
+								"<KerberosServer></KerberosServer>"+
+								"</CifsParams>"+
+							"</Cifs>"+
+						"</TxProfiles>"+
+							"<ScanTray>ADF</ScanTray>"+
+							"<ColorMode>Color</ColorMode>"+
+							"<Resolution>Normal</Resolution>"+
+							"<FileType>PDF</FileType>"+
+					"</IoScanAndSend>"+
+						"<DisplayInfo>"+ 
+							"<Script>"+ 
+								"<![CDATA[<?xml version='1.0' encoding='utf-8'?>"+
+									"<UiScreen>"+
+										"<NullScreen></NullScreen>"+
+									"</UiScreen>" +
+								"]]>"+ 
+							"</Script>"+ 
+						"</DisplayInfo>"+ 
+				"</SerioCommands>";
+	}
+	
+	@POST
+	@Produces("application/xml")
+	@Path("menue")
 	public String login(){
 		return 	
 				"<?xml version='1.0' encoding='utf-8'?>"+
-				"<SerioCommands version='1.0'>"+
-					"<DisplayForm>"+
-						"<Script>"+
-							"<![CDATA["+
-								"<UiScreen infoLevel='Normal'>"+
-									"<Title>Hello BSI.</Title>"+
-									"<AutoTransition>300</AutoTransition>"+
-										"<Operations>"+
-											"<Op type='Submit'  action='http://172.16.100.196:8080/brobsi/brobsi/scannen'>"+
-												"<Label>Submit</Label>"+
-											"</Op>"+
-											"<Op type='Back'  action='http://172.16.100.196:8080/brobsi/brobsi/loginnew'>"+
-												"<Label>Back</Label>"+
-											"</Op>"+
-										"</Operations>"+
-									"<IoScreen>"+
-										"<IoObject>"+
-											"<Title>Welcome To BSI</Title>"+
-											"<Description>Hello BSI World!</Description>"+
-											"<Message>Press OK or Start to proceed</Message>"+
-										"</IoObject>"+
-									"</IoScreen>"+
-								"</UiScreen>"+
-							"]]>"+
-						"</Script>"+
-					"</DisplayForm>"+
-		"</SerioCommands>";
+					"<SerioCommands version='1.0'>"+
+						"<DisplayForm>"+
+							"<Script>"+
+								"<![CDATA["+
+									"<UiScreen>"+
+										"<Title>MFC Jobs</Title>"+
+											"<LinkScreen>"+
+												"<LinkControl>"+
+													"<LinkItem  href='http://172.16.100.196:8080/brobsi/brobsi/loginnew/scanning'>"+
+														"<Label>Scannen</Label>"+
+													"</LinkItem>"+
+													"<LinkItem href='./2.xml'>"+
+														"<Label>Copy</Label>"+
+													"</LinkItem>"+
+													"<LinkItem href='./3.xml'>"+
+														"<Label>Print</Label>"+
+													"</LinkItem>"+
+													"<LinkItem href='./4.xml'>"+
+														"<Label>Fax</Label>"+
+													"</LinkItem>"+
+												"</LinkControl>"+
+											"</LinkScreen>"+
+									"</UiScreen>"+
+								"]]>"+
+							"</Script>"+
+						"</DisplayForm>"+
+					"</SerioCommands>";
+				
+				
+				
+//				"<?xml version='1.0' encoding='utf-8'?>"+
+//				"<SerioCommands version='1.0'>"+
+//					"<DisplayForm>"+
+//						"<Script>"+
+//							"<![CDATA["+
+//								"<UiScreen infoLevel='Normal'>"+
+//									"<Title>Hello BSI.</Title>"+
+//									"<AutoTransition>300</AutoTransition>"+
+//										"<Operations>"+
+//											"<Op type='Submit'  action='http://172.16.100.196:8080/brobsi/brobsi/scannen'>"+
+//												"<Label>Submit</Label>"+
+//											"</Op>"+
+//											"<Op type='Back'  action='http://172.16.100.196:8080/brobsi/brobsi/loginnew'>"+
+//												"<Label>Back</Label>"+
+//											"</Op>"+
+//										"</Operations>"+
+//									"<IoScreen>"+
+//										"<IoObject>"+
+//											"<Title>Welcome To BSI</Title>"+
+//											"<Description>Hello BSI World!</Description>"+
+//											"<Message>Press OK or Start to proceed</Message>"+
+//										"</IoObject>"+
+//									"</IoScreen>"+
+//								"</UiScreen>"+
+//							"]]>"+
+//						"</Script>"+
+//					"</DisplayForm>"+
+//		"</SerioCommands>";
 				
 				
 				
